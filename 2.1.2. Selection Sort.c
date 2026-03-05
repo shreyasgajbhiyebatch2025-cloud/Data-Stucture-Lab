a7ffa63bb31116392f223b9db890312c4dc6efe7
shreyasgajbhiyebatch2025-cloud <shreyas.gajbhiye.batch2025@sitnagpur.siu.edu.in>
2026-03-05 14:50:35 +0530

    2.1.2. Selection Sort.c

---
+++ 2.1.2. Selection Sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int maxIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[maxIndex]) {
+                maxIndex = j;
+            }
+        }
+        swap(&arr[i], &arr[maxIndex]);
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    selectionSort(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
