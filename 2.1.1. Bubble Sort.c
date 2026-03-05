6608f51e00ac31d4deb88627d4dca478d450e887
shreyasgajbhiyebatch2025-cloud <shreyas.gajbhiye.batch2025@sitnagpur.siu.edu.in>
2026-03-05 14:48:11 +0530

    Create 2.1.1. Bubble Sort.c

---
+++ 2.1.1. Bubble Sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int swapped = 1;
+    int i, temp;
+    while (swapped) {
+        swapped = 0;
+        for (i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = 1;
+            }
+        }
+        n--; 
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
