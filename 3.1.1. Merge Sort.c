fa3576d8b6920ca0b9ef4170e43524d6a00e8c03
shreyasgajbhiyebatch2025-cloud <shreyas.gajbhiye.batch2025@sitnagpur.siu.edu.in>
2026-03-09 12:00:20 +0530

    Create 3.1.1. Merge Sort.c

---
+++ 3.1.1. Merge Sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void merge(int arr[], int temp[], int left, int mid, int right) {
+    int i = left, j = mid + 1, k = left;
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) temp[k++] = arr[i++];
+        else temp[k++] = arr[j++];
+    }
+    while (i <= mid) temp[k++] = arr[i++];
+    while (j <= right) temp[k++] = arr[j++];
+    for (i = left; i <= right; i++) arr[i] = temp[i];
+}
+
+void mergeSort(int arr[], int n) {
+    int temp[n];
+    for (int size = 1; size < n; size *= 2) {
+        for (int left = 0; left < n - size; left += 2 * size) {
+            int mid = left + size - 1;
+            int right = (left + 2 * size - 1 < n - 1) ? left + 2 * size - 1 : n - 1;
+            merge(arr, temp, left, mid, right);
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    printArray(arr, n);
+    mergeSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
